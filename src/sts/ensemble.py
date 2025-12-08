f7b58f6b3ab84ceef72544024bd49fbcd277e3d0
burak <95357428+BurakYildizGameDev@users.noreply.github.com>
2025-12-08 19:01:16 +0300

    Compute features and probabilities for new pairs

--- src/sts/ensemble.py
+++ src/sts/ensemble.py
@@ -16,3 +16,18 @@ class Ensemble:
         self.columns = bundle["features"]
         self.minilm = features.load_minilm(self.device)
         self.lstm, self.vocab, self.max_len = lstm.load(bundle["lstm"], self.device)
+
+    def features(self, texts_a, texts_b):
+        texts_a, texts_b = list(texts_a), list(texts_b)
+        cols = {
+            "minilm_sim": features.embedding_similarity(self.minilm, texts_a, texts_b),
+            "lstm_prob": lstm.predict(self.lstm, self.vocab.encode(texts_a, self.max_len),
+                                      self.vocab.encode(texts_b, self.max_len), self.device),
+            "tfidf_sim": features.tfidf_similarity(self.tfidf, texts_a, texts_b),
+        }
+        cols.update(features.lexical_features(texts_a, texts_b))
+        return pd.DataFrame(cols)[self.columns]
+
+    def predict_proba(self, texts_a, texts_b):
+        X = self.features(texts_a, texts_b)
+        return self.meta.predict_proba(X.values)[:, 1], X
